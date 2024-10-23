8675817dd7857c9b046db26afc2658b81c1ad063
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2024-10-23 09:14:03 +0100

    Initial Commit

---
+++ Caller/CallerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallerApp.swift
+//  Caller
+//
+//  Created by Kod Enigma on 23/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct CallerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
